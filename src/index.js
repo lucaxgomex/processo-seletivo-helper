3ad25a9373a4a6ac5fea746373dc720fbb5927c9
lucaxgomex <luc.g.oliv.dev@gmail.com>
2020-12-21 02:45:58 -0300

    Criação de uma página de opções.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+function mostraCadastro() {
+    window.location.href = "../sample.html";
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+function mostraCadastro() {
+	window.location.href = "../sample.html";
+}
